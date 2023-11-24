fdc943bc797b83d0be670e4f57d9859c194c648e
Kiarra Julien <kiarrajulien@yahoo.com>
2023-11-23 21:05:57 -0500

    Initial Commit

---
+++ Julien Family Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Julien Family Recipes
+//
+//  Created by Kiarra Julien on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Julien Family Recipes/Julien_Family_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Julien_Family_RecipesApp.swift
+//  Julien Family Recipes
+//
+//  Created by Kiarra Julien on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Julien_Family_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
